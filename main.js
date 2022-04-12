4619000c86b31d1b6b6884c40ac21a265a778969
Giulia-92 <cionco.giulia@gmail.com>
2022-04-12 16:34:45 +0200

    js-social-posts

--- main.js
+++ main.js
@@ -55,3 +55,50 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+const postContainer = document.querySelector('#container');
+console.log(postContainer);
+
+stampaPost();
+
+function stampaPost(){
+    //console.log('stampaPost');
+	//const card = postContainer.getElementsByClassName('.post');
+    let item = '';
+    for(let i = 0; i < posts.length; i++ ){
+		item +=  `  <div class="post">
+            <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${posts[i].image}" alt=" ${posts[i].name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author"> ${posts[i].name}</div>
+                    <div class="post-meta__time"> ${posts[i].created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text"> ${posts[i].content}</div>
+        <div class="post__image">
+            <img ${posts[i].image}
+            src=" ${posts[i].media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    ` 
+    }
+    postContainer.innerHTML = item; 
+}
